8cd8af06057a720b4b40221579e156b0d965da4d
Elena <estafeliz1102@hotmail.com>
2024-07-27 22:31:49 +0200

    new projects added, ready for deploy

--- src/helpers/projectsList.js
+++ src/helpers/projectsList.js
@@ -1,10 +1,11 @@
 import project01 from "../img/projects/project01.png";
 import project02 from "../img/projects/project 02.png";
+import project03 from "../img/projects/project03.png"
 
 const projects = [
   {
     title: "Ukrainians in Barcelona",
-    skills: "JavaScript, React.js, DataBases, SQL, HTML, CSS",
+    skills: "JavaScript, React.js, DataBases, SQL, Node, API, HTML, CSS",
     img: project01,
     gitHubLink:
       "https://github.com/ElenaPetrash/Ukranians-in-Barcelona.Final-project",
@@ -14,7 +15,14 @@ const projects = [
     skills: "TypeScript, React.js, DataBases, SQL, HTML, CSS",
     img: project02,
     gitHubLink:
-      "https://github.com/ElenaPetrash/Ukranians-in-Barcelona.Final-project",
+      "https://github.com/ElenaPetrash/MusicWay",
+  },
+  {
+    title: "My Portfolio",
+    skills: "JavaScript, React.js, HTML, CSS, Canva",
+    img: project03,
+    gitHubLink:
+      "https://github.com/ElenaPetrash/My-portfolio",
   },
 ];
 
