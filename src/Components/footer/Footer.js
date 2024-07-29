0be389a011cb067f9c4301b8aeb1e31d97ae9f7c
Elena <estafeliz1102@hotmail.com>
2024-07-29 16:47:59 +0200

    links to social media added

--- src/Components/footer/Footer.js
+++ src/Components/footer/Footer.js
@@ -1,50 +1,38 @@
-import "./style.css"
+import "./style.css";
 
-import vk from "../../img/icons/vk.svg"
-import inst from "../../img/icons/instagram.svg"
-import twitter from "../../img/icons/twitter.svg"
-import linkedIn from "../../img/icons/linkedIn.svg"
-import gitHub from "../../img/icons/gitHub.svg"
+import inst from "../../img/icons/instagram.svg";
+import linkedIn from "../../img/icons/linkedIn.svg";
+import gitHub from "../../img/icons/gitHub.svg";
 
 function Footer() {
-    return ( 
-        <footer className="footer">
-        <div className="container">
-          <div className="footer__wrapper">
-            <ul className="social">
-              <li className="social__item">
-                <a href="#!">
-                  <img src={vk} alt="Link" />
-                </a>
-              </li>
-              <li className="social__item">
-                <a href="#!">
-                  <img src={inst} alt="Link" />
-                </a>
-              </li>
-              <li className="social__item">
-                <a href="#!">
-                  <img src={twitter} alt="Link" />
-                </a>
-              </li>
-              <li className="social__item">
-                <a href="#!">
-                  <img src={gitHub} alt="Link" />
-                </a>
-              </li>
-              <li className="social__item">
-                <a href="#!">
-                  <img src={linkedIn} alt="Link" />
-                </a>
-              </li>
-            </ul>
-            <div className="copyright">
-              <p>© 2022 frontend-dev.com</p>
-            </div>
+  return (
+    <footer className="footer">
+      <div className="container">
+        <div className="footer__wrapper">
+          <ul className="social">
+            <li className="social__item">
+              <a href="https://github.com/ElenaPetrash">
+                <img src={gitHub} alt="Link" />
+              </a>
+            </li>
+            <li className="social__item">
+              <a href="https://linkedin.com/in/elena-petrash-37976826a">
+                <img src={linkedIn} alt="Link" />
+              </a>
+            </li>
+            <li className="social__item">
+              <a href="https://www.instagram.com/elenaa_petrash/">
+                <img src={inst} alt="Link" />
+              </a>
+            </li>
+          </ul>
+          <div className="copyright">
+            <p>© 2022 frontend-dev.com</p>
           </div>
         </div>
-      </footer>
-     );
+      </div>
+    </footer>
+  );
 }
 
 export default Footer;
