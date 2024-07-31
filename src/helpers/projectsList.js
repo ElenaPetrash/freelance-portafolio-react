de2c6214d0ba2e44a61e0cde7544171c361c75e5
Elena <estafeliz1102@hotmail.com>
2024-07-31 21:06:01 +0200

    imgs converted to png

--- src/helpers/projectsList.js
+++ src/helpers/projectsList.js
@@ -1,6 +1,6 @@
-import project01 from "../img/projects/project01.png";
-import project02 from "../img/projects/project 02.png";
-import project03 from "../img/projects/project03.png"
+import project01 from "../img/projects/project01.jpg";
+import project02 from "../img/projects/project_02.jpg";
+import project03 from "../img/projects/project03.jpg"
 
 const projects = [
   {
